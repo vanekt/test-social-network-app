94f0250c8d66e4f9a4e6961d0d846529c7cf5d7c
Ivan Tkachenko <vanekt@yandex.ru>
2018-08-15 23:04:37 +0300

    avatar placeholder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import avatarPlaceholder from '../../img/avatar.jpg';
 
 export default ({ avatar, userId, username, logout }) => {
+  const userAvatar = avatar ? avatar : avatarPlaceholder;
   const userArea = userId ? (
     <div>
-      <img src={avatar} title={username} alt={username} />
+      <img src={userAvatar} title={username} alt={username} />
       <button type="button" onClick={logout}>
         Exit
       </button>
