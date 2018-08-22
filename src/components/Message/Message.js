69e741e7d6bd93401e34c94bc14bf4991d2b142e
Ivan Tkachenko <vanekt@yandex.ru>
2018-08-23 01:34:51 +0300

    Message styles improvements

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -5,13 +5,11 @@ import styles from './Message.module.scss';
 
 const Message = ({ data, datetime, authorId, authorName, authorImage, onlyMessage }) => (
   <div className={cn(styles.message, { [styles.onlyMessage]: onlyMessage })}>
-    <NavLink to={`/profile/${authorId}`} className={styles.avatarLink}>
-      <img
-        src={authorImage}
-        className={cn(styles.avatar, { [styles.avatarHidden]: onlyMessage })}
-        alt={authorName}
-        title={authorName}
-      />
+    <NavLink
+      to={`/profile/${authorId}`}
+      className={cn(styles.avatarLink, { [styles.avatarLinkHidden]: onlyMessage })}
+    >
+      <img src={authorImage} className={styles.avatar} alt={authorName} title={authorName} />
     </NavLink>
     <div className={styles.messageBody}>
       {onlyMessage ? null : (
